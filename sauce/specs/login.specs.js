02c25bd8671c679df3dea70d9575c1fcd3983a5c
unknown <mdshakilm2111@gmail.com>
2024-11-09 20:47:11 +0600

    login by locked out user

--- sauce/specs/login.specs.js
+++ sauce/specs/login.specs.js
@@ -6,11 +6,11 @@ const password = "secret_sauce";
 describe("suace site login journey", () => {
     it("Should able to successfully login", async () => {
         await lockedOutActions.enterUserName(userName);
-        await browser.pause(5000);
+        await browser.pause(2000);
         await lockedOutActions.enterPassword(password);
-        await browser.pause(5000);
+        await browser.pause(2000);
         await lockedOutActions.clickOnLoginButton();
-        await browser.pause(5000);
+        await browser.pause(2000);
     });
 
 
